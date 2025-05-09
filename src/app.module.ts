c919871e3d8ca9965e508390bc7628dbfee89f4b
humilimo <lfoc@cin.ufpe.br>
2025-05-09 19:10:52 -0300

    ✨ feat: add DumpsterModule to app.module.ts for enhanced dumpster management functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { ChatbotModule } from './chatbot/service/chatbot.module';
 import { UsersModule } from './users/users.module';
 import { ComplaintModule } from './complaint/complaint.module';
+import { DumpsterModule } from './dumpster/dumpster.module';
 @Module({
   imports: [
     ChatbotModule, 
     UsersModule,
-    ComplaintModule
+    ComplaintModule,
+    DumpsterModule
   ],
   controllers: [AppController],
   providers: [AppService],
